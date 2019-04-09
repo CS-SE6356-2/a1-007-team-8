75b3eac17989197bd3a4f5e90abd924cbf12ec41
WScottJacobsen <jacobsenscott07@gmail.com>
2019-04-09 18:24:32 -0500

    configure gradle

---
+++ src/main/java/gofish/models/Player.java
@@ -0,0 +1,33 @@
+class Player {
+	private String name;
+	private Hand hand;
+	private int score, wins;
+
+	public Player() {
+		this.name = "Player";
+		this.hand = new Hand();
+		this.score = 0;
+		this.wins = 0;
+	}
+
+	public Player(String name) {
+		this();
+		this.name = name;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public Hand getHand() {
+		return this.hand;
+	}
+
+	public int getScore() {
+		return this.score;
+	}
+
+	public int getWins() {
+		return this.wins;
+	}
+}
