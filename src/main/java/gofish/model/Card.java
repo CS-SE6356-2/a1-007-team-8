c3d9ecb9f2893f3d9a90471f4f905db3867f0281
Michael Loney <mloney@protonmail.com>
2019-04-09 19:28:04 -0500

    Create Hand class

--- src/main/java/gofish/model/Card.java
+++ src/main/java/gofish/model/Card.java
@@ -4,6 +4,8 @@ class Card {
     int value;
     String suit, rank;
 
+    // Constructor
+
     public Card(String suit, String rank, int value) throws IllegalArgumentException {
         // Check if requested card is valid
         if (!validateCard(suit, rank)) {
@@ -15,6 +17,7 @@ class Card {
     }
 
     // Accessors
+
     public int getValue() {
         return value;
     }
@@ -32,7 +35,14 @@ class Card {
         do something
     }*/
 
-    // Validates that a card's rank/suit exists
+    // Helpers
+
+    /**
+     * Validates that a card's rank/suit exists
+     * @param   suit    suit of requested card
+     * @param   rank    rank of requested card
+     * @return          true of requested suit and rank are valid
+     */
     private boolean validateCard(String suit, String rank) {
         final String[] SUITS = {"club", "spade", "diamond", "heart"};
         final String[] RANKS = {"ace", "king", "queen", "jack", "joker", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"};
