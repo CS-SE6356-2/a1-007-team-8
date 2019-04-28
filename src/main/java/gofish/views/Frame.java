5a421ab4add11cdcf1121358db2f3ffa47b06e7a
Mjone77 <Mjone773@gmail.com>
2019-04-27 21:35:50 -0500

    Create main class with a basic Frame

--- src/main/java/gofish/views/Frame.java
+++ src/main/java/gofish/views/Frame.java
@@ -2,13 +2,17 @@ package gofish.views;
 
 import gofish.models.Player;
 
-public class Frame {
+import javax.swing.*;
+
+public class Frame extends JFrame {
     private int width, height, backgroundColor;
 
-    public Frame(int width, int height, int bgColor) {
+    public Frame(String name, int width, int height, int bgColor) {
+        super(name);
         this.width = width;
         this.height = height;
         backgroundColor = bgColor;
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
     public void render() {}
